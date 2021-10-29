b30182a4d02da0c8d1d8bc3d5553b66e04ac7f32
Sam Place <samvauclain@gmail.com>
2021-10-29 13:48:01 -0700

    Got a working timer, need to connect it to button and test a little more

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+// FIRST TO-DO: setup timer
+var timerEl = document.getElementById('timer');
+timerEl.textContent = "Time: " + 0;
+
+// Timer that counts down from 75
+function timer() {
+    var timeRemaining = 3;
+  
+    // every second (1000 milliseconds, second argument of setInterval & set at end of anonymous function)
+    var timeInterval = setInterval(function () {
+  
+      // if timer has 1 or more seconds left, subtract one every second and display new value
+      if (timeRemaining >= 0) {
+        timerEl.textContent = "Time: " + timeRemaining;
+        timeRemaining--;
+        console.log(timeRemaining);
+      } 
+      // otherwise, reset time interval, 
+      else {
+        clearInterval(timeInterval);
+        timerEl.textContent = "Time: " + 0;
+        timeExpired();
+      }
+    }, 1000);
+  }
+  
+  // Setup what happens when time runs out.
+  function timeExpired() {
+    alert('Time Expired.');
+  }
+
+  // Run the timer function, this needs to be hooked to the "Start Quiz" button.
+  timer();
+
+
+// SECOND TO-DO: I'll try setting up the quiz questions and related elements as objects, make them flexible so they could be used for everything after the "menu". 
+// Arrays within the objects for questions and answers? 
+//Populate answer buttons with their text and true / false logic.  
+
+
 // alert("script is linked!");
 
 // AS A coding boot camp student
