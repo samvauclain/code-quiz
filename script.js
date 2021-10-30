4a5524298541acb2ddb75fabe97b3a52fb2f3915
Sam Place <samvauclain@gmail.com>
2021-10-30 10:49:53 -0700

    dynamically generated buttons are working when clicked  now

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ var timerEl = document.getElementById('timer');
 timerEl.textContent = 'Time: ' + 0;
 
 // Select HTML eleents and store as variables for populateQuizObjects();
-var quizContainer = document.getElementById('startButton');
+var quizContainer = document.getElementById('quizContainer');
 var startButton = document.getElementById('startButton');
 var quizButtonGroup = document.getElementById('quizButtonGroup');
 var quizParagraph = document.getElementById('quizParagraph');
 var quizHeader = document.getElementById('quizHeader');
 
-
+//  __________
 // Question and Answer objects (start with 1 in a simple form)
+// dynamically generate the answer numbers too
 var quizObject1 = {
-    question : 'Question 1 __________',
-    answer : ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
-    result : [false, true, false, false],
+    question : 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answer : ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
+    result : [false, false, false, true],
     message : ['Wrong!', 'Correct!']
 }
 
 // Timer that will count down from 75, currently set lower for testing
 function timer() {
-    var timeRemaining = 3;
+    var timeRemaining = 10;
   
     // every second (1000 milliseconds, second argument of setInterval & set at end of anonymous function)
     var timeInterval = setInterval(function () {
@@ -41,42 +42,46 @@ function timer() {
   }
 
   function populateQuizObjects() {
-    //remove start button and quiz paragraph for "clean slate"
-    startButton.remove();
-    quizParagraph.remove();
+      //remove start button and quiz paragraph for "clean slate"
+      startButton.remove();
+      quizParagraph.remove();
 
-    // Replace header text with question 1 for now
-    quizHeader.textContent = quizObject1.question;
-    // removing class to put text back to left isn't working
-    quizContainer.classList.remove('text-center');
+      // Replace header text with question 1 for now
+      quizHeader.textContent = quizObject1.question;
+      // removing class to put text back to left isn't working
+      quizContainer.classList.remove('text-center');
 
+    // var answerCount = 0;
     // Loop through and put question buttons into quizButtonGroup div (where start button was)
-    for (var i = 0; i < quizObject1.answer.length; i++) {
-        quizButtonGroup.innerHTML += '<button type="button" class="d-block mt-2 btn btn-primary">' + quizObject1.answer[i] + '</button>';
-        console.log(quizObject1.result[i]);
-    }
-
-    // need this to be part of the for loop too
-    // if (quizObject1.answer[i] === true) {
-    //     alert("Correct! We need to add points.");
-    // }
-    // else {
-    //     alert("False, we need to subtract time");
-    // }
-
+      for (var i = 0; i < quizObject1.answer.length; i++) {
+          quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary" onclick="buttonHandler(${quizObject1.result[i]})">` + quizObject1.answer[i] + '</button>';
+          console.log(quizObject1.result[i]);
+      }
   };
-  
+
+  function buttonHandler(arg) {
+    console.log(arg);
+
+      if (arg) {
+        alert("Correct!");
+      }
+      else {
+        alert("False!");
+      }
+  }
+
   // Run the timer function, this needs to be hooked to the "Start Quiz" button.
-  document.getElementById('startButton').onclick = function(){
+  document.getElementById('startButton').addEventListener("click", function(e){
+    console.log("start button:", e.target)
     timer();
     populateQuizObjects();
-  };
-
+  });
 
   // Setup what happens when time runs out.
   function timeExpired() {
     alert('Time Expired.');
-  };
+  }
+ 
 
 
 // SECOND TO-DO: I'll try setting up the quiz questions and related elements as objects, make them flexible so they could be used for everything after the "menu". 
