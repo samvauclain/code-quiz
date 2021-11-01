01d1181aefae304b1303f9ea7f69926c5a2c84f3
Sam Place <samvauclain@gmail.com>
2021-10-31 18:55:59 -0700

    refactored, much more effecient now

--- script.js
+++ script.js
@@ -18,40 +18,37 @@ var userScoresEl = '';
 var userHighScore = [];
 var highScores = [];
 var newScore = '';
+var index = 0;
 
 //  __________
 // Question and Answer objects (start with 1 in a simple form)
 // dynamically generate the answer numbers too
 
-var quizObject1 = {
+var quizObjectArray = [{
   question : 'Commonly used data types DO NOT include __________',
   answer : ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
   result : [false, false, true, false]
-}
-
-var quizObject2 = {
-  question : 'The condition in an if / else statement is enclosed with __________',
-  answer : ['1. quotes', '2. curly brackets', '3. parentheses', '4. square brackets'],
-  result : [false, false, true, false]
-}
-
-var quizObject3 = {
-  question : 'Arrays in JavaScript can be used to store __________',
-  answer : ['1. numbers and strings', '2. other arrarys', '3. booleans', '4. all of the above'],
-  result : [false, false, false, true]
-}
-
-var quizObject4 = {
-  question : 'String values must be enclosed within __________ when being assigned to variables.',
-  answer : ['1. commas', '2. curly brackets', '3. quotes', '4. parentheses'],
-  result : [false, false, true, false]
-}
-
-var quizObject5 = {
-  question : 'A very useful tool used during development and debugging for printing content to the debugger is:',
-  answer : ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
-  result : [false, false, false, true]
-}
+  },
+  {
+    question : 'The condition in an if / else statement is enclosed with __________',
+    answer : ['1. quotes', '2. curly brackets', '3. parentheses', '4. square brackets'],
+    result : [false, false, true, false]
+  },
+  {
+    question : 'Arrays in JavaScript can be used to store __________',
+    answer : ['1. numbers and strings', '2. other arrarys', '3. booleans', '4. all of the above'],
+    result : [false, false, false, true]
+  },
+  {
+    question : 'String values must be enclosed within __________ when being assigned to variables.',
+    answer : ['1. commas', '2. curly brackets', '3. quotes', '4. parentheses'],
+    result : [false, false, true, false]
+  },
+  {
+    question : 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answer : ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
+    result : [false, false, false, true]
+  }]
 
 // Timer that will count down from 75, currently set lower for testing
 function timer() {
@@ -83,88 +80,28 @@ function timer() {
   function populateQuizObjects() {
       quizContainer.classList.remove('text-center');
 
-    // var answerCount = 0;
-    // Loop through and put question buttons into quizButtonGroup div (where start button was)
-    if (answersGiven===0) {
-      populate1();
-    }
-    else if (answersGiven===1) {
-      populate2();
-    }
-    else if (answersGiven===2) {
-      populate3();
-    }
-    else if (answersGiven===3) {
-      populate4();
-    }
-    else if (answersGiven===4) {
-      populate5();
-    }
-    else {
-      // alert('quiz done');
-      allDone();
-    }
+      if (answersGiven === quizObjectArray.length - 1) {
+        allDone();
+        return;
+      }
 
+      populate();
   };
 
-  function populate1() {
-      // Replace header text with question 1 for now
-      quizHeader.textContent = quizObject1.question;
-
-      //remove start button and quiz paragraph for "clean slate"
-      startButton.remove();
-      quizParagraph.remove();
-
-    for (var i = 0; i < quizObject1.answer.length; i++) {
-      quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary questionBtn" onclick="buttonHandler(${quizObject1.result[i]})">` + quizObject1.answer[i] + '</button>';
-      // console.log(quizObject1.result[i]);
-    }
-
-  };
-
-  function populate2() {
-    quizButtonGroup.innerHTML = '';
-
+  function populate() {
     // Replace header text with question 1 for now
-    quizHeader.textContent = quizObject2.question;
-
-    for (var i = 0; i < quizObject2.answer.length; i++) {
-      quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary questionBtn" onclick="buttonHandler(${quizObject2.result[i]})">` + quizObject2.answer[i] + '</button>';
-    }
-  };
-
-  function populate3() {
+    quizHeader.textContent = quizObjectArray[index].question;
     quizButtonGroup.innerHTML = '';
+    //remove start button and quiz paragraph for "clean slate"
+    startButton.remove();
+    quizParagraph.remove();
 
-    // Replace header text with question 1 for now
-    quizHeader.textContent = quizObject3.question;
-
-    for (var i = 0; i < quizObject3.answer.length; i++) {
-      quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary questionBtn" onclick="buttonHandler(${quizObject3.result[i]})">` + quizObject3.answer[i] + '</button>';
-    }
-  };
-
-  function populate4() {
-    quizButtonGroup.innerHTML = '';
-
-    // Replace header text with question 1 for now
-    quizHeader.textContent = quizObject4.question;
-
-    for (var i = 0; i < quizObject4.answer.length; i++) {
-      quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary questionBtn" onclick="buttonHandler(${quizObject4.result[i]})">` + quizObject4.answer[i] + '</button>';
-    }
-  };
-
-  function populate5() {
-    quizButtonGroup.innerHTML = '';
-
-    // Replace header text with question 1 for now
-    quizHeader.textContent = quizObject5.question;
-
-    for (var i = 0; i < quizObject5.answer.length; i++) {
-      quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary questionBtn" onclick="buttonHandler(${quizObject5.result[i]})">` + quizObject5.answer[i] + '</button>';
-    }
-  };
+  for (var i = 0; i < quizObjectArray[index].answer.length; i++) {
+    quizButtonGroup.innerHTML += `<button type="button" class="d-block mt-2 btn btn-primary questionBtn" onclick="buttonHandler(${quizObjectArray[index].result[i]})">` + quizObjectArray[index].answer[i] + '</button>';
+    // console.log(quizObjectArray.result[i]);
+  }
+  index++; 
+};
 
   function allDone() {
     // Replace header text with question 1 for now
@@ -183,13 +120,14 @@ function timer() {
         userScore = userScore + 5;
       }
       else {
-        quizButtonGroup.innerHTML += "<hr><h3>Wrong! - 10 seconds</h3>";
+        quizButtonGroup.innerHTML += "<hr><h3>Wrong! - 10 seconds/points</h3>";
         timeRemaining = timeRemaining - 10;
       }
 
       document.getElementsByClassName('questionBtn').disabled = true;
 
       answersGiven++;
+      
       setTimeout(function(){
         populateQuizObjects(); 
       }, 1000);
